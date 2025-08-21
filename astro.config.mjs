82a683465bbd856708c7228852872a72a7e335dd
Edu Calvo <educlopez93@gmail.com>
2025-08-21 17:12:58 +0200

    initial

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@astrojs/react";
+
+export default defineConfig({
+  integrations: [mdx(), react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
---
+++ src/content/config.ts
@@ -0,0 +1,36 @@
+import { defineCollection, z } from "astro:content";
+import { glob } from "astro/loaders";
+const projects = defineCollection({
+  loader: glob({ pattern: "**/*.md", base: "./src/content/projects" }),
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      description: z.string(),
+      cover: image(),
+      coverAlt: z.string(),
+      logo: z.object({
+        image: image(),
+        fallback: z.object({
+          text: z.string().length(1),
+          bgColor: z.string(),
+        }),
+      }),
+      caseStudy: z.object({
+        challenge: z.string(),
+        solution: z.string(),
+        results: z.array(z.string()),
+        links: z
+          .array(
+            z.object({
+              text: z.string(),
+              url: z.string().url(),
+            })
+          )
+          .optional(),
+      }),
+    }),
+});
+
+export const collections = {
+  projects,
+};
